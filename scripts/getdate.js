0f0fc82e7ba81bd54568ec33d8b7169b75c71765
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2023-09-27 17:38:52 -0600

    updated home page

--- scripts/getdate.js
+++ scripts/getdate.js
@@ -1,3 +1,10 @@
 let today = new Date();
-document.querySelector("#currentYear").textContent=today.getFullYear()
-document.querySelector("#lastModified").textContent=document.lastModified
+document.querySelector("#currentYear").textContent=today.getFullYear();
+document.querySelector("#lastModified").textContent=document.lastModified;
+
+let hambutton = document.querySelector("#menu");
+let navigation = document.querySelector(".navigation");
+hambutton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hambutton.classList.toggle("open");
+})
