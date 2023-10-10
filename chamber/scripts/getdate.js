464640f2ad863172f1c442e95d03cff3c69ac444
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2023-10-09 22:05:50 -0600

    finished home page

--- chamber/scripts/getdate.js
+++ chamber/scripts/getdate.js
@@ -4,3 +4,7 @@ hambutton.addEventListener("click", () => {
     navigation.classList.toggle("open");
     hambutton.classList.toggle("open");
 })
+
+let today = new Date();
+document.querySelector("#currentYear").textContent=today.getFullYear();
+document.querySelector("#lastModified").textContent=document.lastModified;
