35c91476973a1e521ab811cb8585abade2e09fb7
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2023-10-31 18:00:19 -0600

    finished discover page

--- chamber/scripts/daysSinceLastVisit.js
+++ chamber/scripts/daysSinceLastVisit.js
@@ -4,9 +4,24 @@ const todayDate = new Date();
 
 if (localStorage.getItem("lastVisit"))
 {
+    let lastVisit = Number(localStorage.getItem("lastVisit"));
+    let daysBetweenVisits = (todayDate.getTime() - lastVisit) / msToDays;
 
+    if (daysBetweenVisits < 1)
+    {
+        message.textContent = "Back so soon! Awesome!";
+    }
+    else if (daysBetweenVisits == 1)
+    {
+        message.textContent = "You last visited 1 day ago.";
+    }
+    else
+    {
+        message.textContent = "You last visited " + daysBetweenVisits.toFixed(0) + " days ago.";
+    }
 }
 else
 {
+    message.textContent = "Welcome! Let us know if you have any questions."
     localStorage.setItem("lastVisit",todayDate.getTime());
 }
