76dad456d0b34c927e2d8b86ed8520e82997934c
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2023-11-16 16:40:38 -0700

    finished weather activity for home page

--- chamber/scripts/hideBanner.js
+++ chamber/scripts/hideBanner.js
@@ -1,6 +1,18 @@
 const banner = document.querySelector("#banner");
 const hideButton = document.querySelector("#banner-button");
 
+function displayBanner()
+{
+    let today = new Date();
+    let dayOfWeek = today.getDay();
+    if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3) 
+    {
+        banner.classList.add("show");
+        hideButton.classList.add("show");
+    }
+}
+
+displayBanner();
 hideButton.addEventListener('click', () => {
     banner.classList.toggle("hide");
     hideButton.classList.toggle("hide");
